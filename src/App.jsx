0531371bc7f9971f63356acfbf68408b740f2543
aerocanlas <princeaero.canlas.cics@ust.edu.ph>
2024-01-09 16:33:11 +0800

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Typography } from '@mui/material'
+import ResponsiveAppBar from './components/ResponsiveAppBar'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from '@mui/material/Toolbar';
 
 const App = () => {
+
+  const theme = createTheme({
+    palette: {
+      background: {
+        default: "#f4f4f4"
+      }
+    }
+   });
+
   return (
-    <div>
-        <Typography variant='h1'>Hello World!</Typography>
-        </div>
+    <>
+       <ThemeProvider theme={theme}>
+     <CssBaseline />
+     <ResponsiveAppBar position="static" color="transparent">
+   </ResponsiveAppBar>
+   </ThemeProvider>
+</>
   )
 }
 
---
+++ src/components/ResponsiveAppBar.jsx
@@ -0,0 +1,150 @@
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  createTheme,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+
+const Navbar = () => {
+  const pages = ['Home', 'Contact', 'Community'];
+  const pages1 = ['Wholesale', 'Retail'];
+  const [open, setOpen] = useState(false);
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const theme = createTheme ({
+    Typography: [
+      'Arimo',
+      'sans-serif',
+    ].join(',')
+  })
+
+  return (
+    <AppBar position="sticky" color="transparent" sx={{boxShadow: 'none', marginTop: '60px'}}>
+      <StyledToolbar >
+      <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center" >{page}</Typography>
+                </MenuItem>
+                ))}
+                {pages1.map((page1) => (
+                <MenuItem key={page1} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page1}</Typography>
+                </MenuItem>
+                ))}
+            </Menu>
+          </Box>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "flex" }, marginLeft: '100px', marginRight: '-150px'}}>
+        {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'black', display: 'block', textTransform: 'none' }}
+              >
+                {page}
+              </Button>
+            ))}
+        </Typography>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "flex" }, fontFamily: 'Arimo', fontWeight: 500, color: 'black' }}>
+          THE STREET MARKET <Typography variant="h6" sx={{ display: { xs: "none", sm: "flex" }, fontSize: '8px', }}>TM</Typography>
+        </Typography>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "flex" }, marginLeft: '-100px', marginRight: '150px' }}>
+        {pages1.map((page1) => (
+              <Button
+                key={page1}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'black', display: 'block', textTransform: 'none' }}
+              >
+                {page1}
+              </Button>
+            ))}
+        </Typography>
+        <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'Arimo',
+              fontWeight: 500,
+              letterSpacing: '0px',
+              color: 'black',
+              fontSize: '18px',
+              textDecoration: 'none',
+            }}
+          >
+            THE STREET MARKET <Typography variant="h6" sx={{ display: { xs: "flex", sm: "flex" }, fontSize: '8px', }}>TM</Typography>
+          </Typography>
+      </StyledToolbar>
+      
+    </AppBar>
+  );
+};
+
+export default Navbar;
