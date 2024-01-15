d03ec33cd27f2e77ef79c2e84d2d639a3408c1f6
Sofhia Lacandola <biancaxsofhia@gmail.com>
2024-01-15 22:48:29 +0800

    community page added

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Typography,  Grid, Box, Card, CardContent, Image, CardMedia } from '@mui/material'
+import { Typography, Grid, Box, Card, CardContent, Image, CardMedia } from '@mui/material'
 import ResponsiveAppBar from './ResponsiveAppBar'
-import { ThemeProvider, createTheme  } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import ResponsiveFooter from './ResponsiveFooter';
 import { Route, Routes } from 'react-router';
@@ -11,26 +11,28 @@ import Button from '@mui/material/Button';
 
 const Community = () => {
 
-  const theme = createTheme({
-    palette: {
-      background: {
-        default: "#f4f4f4"
-      }
-    }
-   });
+ const theme = createTheme({
+   palette: {
+     background: {
+       default: "#f4f4f4"
+     }
+   }
+  });
 
-  return (
-    <>
-    <title>The Street Market | Community</title>
-       <ThemeProvider theme={theme}>
-     <CssBaseline />
-     <ResponsiveAppBar position="static" color="transparent"/>
-Community
-     
-   <ResponsiveFooter position="bottom"/>
-   </ThemeProvider>
+ return (
+   <>
+   <title>The Street Market | Community</title>
+      <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <ResponsiveAppBar position="static" color="transparent"/>
+    <Box sx={{marginTop: '50px', marginBottom: '64px', maxWidth: ['100%', '100%', '100%', '100%', '100%']}}>
+ <img src="/images/Community-Page-Placeholder-Image.png" alt="My Image" style={{maxWidth: '100%', height: 'auto', objectFit: 'contain'}}/>
+</Box>
+
+    <ResponsiveFooter position="bottom"/>
+  </ThemeProvider>
 </>
-  )
+ )
 }
 
 export default Community
