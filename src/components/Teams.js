6cdd86ee86f2715ed173ea494f01753068dbb502
Sofhia Lacandola <biancaxsofhia@gmail.com>
2024-01-30 23:03:18 +0800

    fixed draggable photos in teams page

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -8,7 +8,6 @@ import ResponsiveFooter from './ResponsiveFooter';
 import { Route, Routes } from 'react-router';
 import FAQs from './faqs';
 import Draggable from 'react-draggable';
-import Navbar from './Navbar';
 
 const App = () => {
 
@@ -28,47 +27,44 @@ const App = () => {
     <title>The Street Market | Teams </title>
        <ThemeProvider theme={theme}>
      <CssBaseline />
-     <Navbar position="static" color="transparent"/>
-
-   <div style={{display: 'flex', height: '100vh', marginTop: '-80px'}}>
+     <ResponsiveAppBar position="static" color="transparent">
+   </ResponsiveAppBar>
    <Draggable>
-   <img src="/images/1.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/8.png" alt="My Image" style={{ marginLeft: '20%', marginTop: '15%',  maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+    </Draggable>
+   <Draggable>
+   <img src="/images/1.png" alt="My Image" style={{ marginLeft: '-9%', marginBottom: '3%',  maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/2.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/2.png" alt="My Image" style={{ marginLeft: '-7.3%', marginBottom: '4%',maxWidth: '16%', height: 'auto', objectFit: 'contain'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/3.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/11.png" alt="My Image" style={{ marginLeft: '-8.3%', marginBottom: '2%', maxWidth: '19.5%', height: 'auto', objectFit: 'contain'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/4.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/3.png" alt="My Image" style={{ marginLeft: '-9.7%', marginBottom: '2.6%', maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/5.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/4.png" alt="My Image" style={{ marginLeft: '-5.2%', marginBottom: '1%', maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/6.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/7.png" alt="My Image" style={{  marginLeft: '18%', marginTop: '-7.5%', maxWidth: '18%', height: 'auto', objectFit: 'contain',  display: 'block'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/7.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/10.png" alt="My Image" style={{  marginLeft: '28.9%', marginTop: '-17%', maxWidth: '18%', height: 'auto', objectFit: 'contain', display: 'block'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/8.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/6.png" alt="My Image" style={{ marginLeft: '38%', marginTop: '-18%', maxWidth: '18%', height: 'auto', objectFit: 'contain', display: 'block'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/9.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/5.png" alt="My Image" style={{  marginLeft: '47.3%', marginTop: '-18.5%', maxWidth: '18%', height: 'auto', objectFit: 'contain', display: 'block'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/10.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/12.png" alt="My Image" style={{  marginLeft: '56.8%', marginTop: '-17%', maxWidth: '17%', height: 'auto', objectFit: 'contain', display: 'block'}}/>
     </Draggable>
     <Draggable>
-   <img src="/images/11.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
+   <img src="/images/9.png" alt="My Image" style={{  marginLeft: '65.5%', marginTop: '-18%', maxWidth: '18%', height: 'auto', objectFit: 'contain', display: 'block'}}/>
     </Draggable>
-    <Draggable>
-   <img src="/images/12.png" alt="My Image" style={{maxWidth: '18%', height: 'auto', objectFit: 'contain'}}/>
-    </Draggable>
-</div>
-
 
   <Typography 
       variant="h3" 
@@ -77,7 +73,7 @@ const App = () => {
         fontSize: { xs: '0.9em', sm: '1rem', md: '1rem', lg: '1.2rem', xl: '1.2rem' },
         fontWeight: 'bold', 
         textAlign: 'center', 
-        marginTop: { xs: '-320px', sm: '100px', md: '-70px', lg: '-50px', xl: '-50px' }, // Adjust as needed
+        marginTop: { xs: '-320px', sm: '100px', md: '-70px', lg: '50px', xl: '-50px' }, // Adjust as needed
         marginBottom: { xs: '50px', sm: '', md: '30px', lg: '90px' }
       }}
   >
